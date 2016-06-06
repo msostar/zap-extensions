b67d1dac0524583c63ecea951bc677021047618b
Martin Šoštar <martin.sostar1@gmail.com>
2016-06-06 10:29:46 +0200

    Password is echoed on web page, finished

--- src/org/zaproxy/zap/extension/asvspscan/Authentication/PasswordEcho.java
+++ src/org/zaproxy/zap/extension/asvspscan/Authentication/PasswordEcho.java
@@ -18,6 +18,7 @@ public class PasswordEcho extends PluginPassiveScanner {
   //  private static Vulnerability vuln = Vulnerabilities.getVulnerability("wasc_20");
     private PassiveScanThread parent = null;
     private static final Logger logger = Logger.getLogger(PasswordEcho.class);
+    private String request="abcdefgijkl";
 
 
 
@@ -25,23 +26,50 @@ public class PasswordEcho extends PluginPassiveScanner {
     public void scanHttpRequestSend(HttpMessage httpMessage, int i) {
 
         String body= httpMessage.getRequestBody().toString();
-        System.out.println(body);
+        String[] bodyParts= body.split("=");
+        int counter=0;
+        for(String bdy: bodyParts){
+            if(bdy.contains("pass") || bdy.contains("psw")){
+                request=bodyParts[counter+1];
+                break;
+            }
+            counter++;
+        }
+        request=request.split("&")[0];
+
+        System.out.println(request);
+
+
+
 
     }
 
     @Override
     public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
 
-        long start = System.currentTimeMillis();
-
-        Vector<String> cookies1 = msg.getResponseHeader().getHeaders(HttpHeader.SET_COOKIE);
 
 
+        String body= msg.getResponseBody().toString();
+        if(body.contains(request)){
 
+            Alert alert = new Alert(getPluginId(), Alert.RISK_LOW, Alert.CONFIDENCE_HIGH, getName());
+            alert.setDetail(
+                    "Password is echoed on web page",
+                    msg.getRequestHeader().getURI().toString(),
+                    "password",    // Param
+                    "", // Attack
+                    "", // Other info
+                    "Change password (password is to weak,to generic or same as username) and check why does it echo on the web page",
+                    "https://www.youtube.com/watch?v=zUM7i8fsf0g\n" +
+                            "https://ckarande.gitbooks.io/owasp-nodegoat-tutorial/content/tutorial/a2_-_password_guessing_attack.html",
+                    "",    // Evidence
+                    0,    // CWE Id
+                    0,    // WASC Id
+                    msg);
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("\tScan of record " + id + " took " + (System.currentTimeMillis() - start) + " ms");
+            parent.raiseAlert(id, alert);
         }
+
 
     }
 
